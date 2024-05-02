05039fe663d03b398ab8d6cdaf730a9c545c1eb7
Sahil Arun <151955025+sahilarun@users.noreply.github.com>
2024-05-02 14:05:34 +0530

    Update class_binding.dart

--- packages/refiy-script/lib/binding/class_binding.dart
+++ packages/refiy-script/lib/binding/class_binding.dart
@@ -277,6 +277,224 @@ class RSRandomClassBinding extends RSExternalClass {
     }
   }
 
+  class RSMathClassBinding extends RSExternalClass {
+  RSMathClassBinding() : super('Math');
+
+  @override
+  dynamic memberGet(String id, {String? from}) {
+    switch (id) {
+      case 'Math.e':
+        return math.e;
+      case 'Math.pi':
+        return math.pi;
+      case 'Math.degrees':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            degrees(positionalArgs.first.toDouble());
+      case 'Math.radians':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            radians(positionalArgs.first.toDouble());
+      case 'Math.radiusToSigma':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            radiusToSigma(positionalArgs.first.toDouble());
+      case 'Math.gaussianNoise':
+        return (RSEntity entity,
+            {List<dynamic> positionalArgs = const [],
+            Map<String, dynamic> namedArgs = const {},
+            List<RSType> typeArgs = const []}) {
+          final mean = positionalArgs[0].toDouble();
+          final standardDeviation = positionalArgs[1].toDouble();
+          final math.Random? randomGenerator = namedArgs['randomGenerator'];
+          assert(standardDeviation > 0);
+          final num? min = namedArgs['min'];
+          final num? max = namedArgs['max'];
+          double r;
+          do {
+            r = gaussianNoise(
+              mean,
+              standardDeviation,
+              randomGenerator: randomGenerator,
+            );
+          } while ((min != null && r < min) || (max != null && r > max));
+          return r;
+        };
+      case 'Math.noise2d':
+        return (RSEntity entity,
+            {List<dynamic> positionalArgs = const [],
+            Map<String, dynamic> namedArgs = const {},
+            List<RSType> typeArgs = const []}) {
+          final int size = positionalArgs[0].toInt();
+          final seed = namedArgs['seed'] ?? math.Random().nextInt(1 << 32);
+          final frequency = namedArgs['frequency'];
+          final noiseTypeString = namedArgs['noiseType'];
+          NoiseType noiseType;
+          switch (noiseTypeString) {
+            case 'perlinFractal':
+              noiseType = NoiseType.perlinFractal;
+            case 'perlin':
+              noiseType = NoiseType.perlin;
+            case 'cubicFractal':
+              noiseType = NoiseType.cubicFractal;
+            case 'cubic':
+            default:
+              noiseType = NoiseType.cubic;
+          }
+          return noise2(
+            size,
+            size,
+            seed: seed,
+            frequency: frequency,
+            noiseType: noiseType,
+          );
+        };
+      case 'Math.min':
+        return (RSEntity entity,
+            {List<dynamic> positionalArgs = const [],
+            Map<String, dynamic> namedArgs = const {},
+            List<RSType> typeArgs = const []}) {
+          if (positionalArgs[0] == null) {
+            return positionalArgs[1];
+          }
+          if (positionalArgs[1] == null) {
+            return positionalArgs[0];
+          }
+          return math.min(positionalArgs[0] as num, positionalArgs[1] as num);
+        };
+      case 'Math.max':
+        return (RSEntity entity,
+            {List<dynamic> positionalArgs = const [],
+            Map<String, dynamic> namedArgs = const {},
+            List<RSType> typeArgs = const []}) {
+          if (positionalArgs[0] == null) {
+            return positionalArgs[1];
+          }
+          if (positionalArgs[1] == null) {
+            return positionalArgs[0];
+          }
+          return math.max(positionalArgs[0] as num, positionalArgs[1] as num);
+        };
+      case 'Math.sqrt':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            math.sqrt(positionalArgs.first as num);
+      case 'Math.pow':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            math.pow(positionalArgs[0] as num, positionalArgs[1] as num);
+      case 'Math.sin':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            math.sin(positionalArgs.first as num);
+      case 'Math.cos':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            math.cos(positionalArgs.first as num);
+      case 'Math.tan':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            math.tan(positionalArgs.first as num);
+      case 'Math.exp':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            math.exp(positionalArgs.first as num);
+      case 'Math.log':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            math.log(positionalArgs.first as num);
+      case 'Math.parseInt':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            int.tryParse(positionalArgs.first as String,
+                radix: namedArgs['radix']) ??
+            0;
+      case 'Math.parseDouble':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            double.tryParse(positionalArgs.first as String) ?? 0.0;
+      case 'Math.sum':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            (positionalArgs.first as List<num>)
+                .reduce((value, element) => value + element);
+      case 'Math.checkBit':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            ((positionalArgs[0] as int) & (1 << (positionalArgs[1] as int))) !=
+            0;
+      case 'Math.bitLS':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            (positionalArgs[0] as int) << (positionalArgs[1] as int);
+      case 'Math.bitRS':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            (positionalArgs[0] as int) >> (positionalArgs[1] as int);
+      case 'Math.bitAnd':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            (positionalArgs[0] as int) & (positionalArgs[1] as int);
+      case 'Math.bitOr':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            (positionalArgs[0] as int) | (positionalArgs[1] as int);
+      case 'Math.bitNot':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            ~(positionalArgs[0] as int);
+      case 'Math.bitXor':
+        return (RSEntity entity,
+                {List<dynamic> positionalArgs = const [],
+                Map<String, dynamic> namedArgs = const {},
+                List<RSType> typeArgs = const []}) =>
+            (positionalArgs[0] as int) ^ (positionalArgs[1] as int);
+
+      default:
+        throw RSError.undefined(id);
+    }
+  }
+}
+
+
   @override
   dynamic instanceMemberGet(dynamic object, String id) =>
       (object as math.Random).htFetch(id);
